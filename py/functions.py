819bff65961e56dffe4955189ea8eeecebe65265
Marcel Ruland <ruland.marcel@gmail.com>
2019-01-20 11:34:35 +0100

    rename python file and add simplify function

---
+++ py/functions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 19 2019
+
+@author: marcelruland
+"""
+
+import pandas as pd
+
+def calculate_medians(filename):
+    df = pd.read_csv(filename)
+    df_medians = df.groupby("signals-interval").median()
+    df_medians.to_csv("df_medians.csv")
+
+
+def simplify(filename, n):
+    """
+    keeps every n-th line in a file
+    """
+    input_file = open(filename, "r")
+    output_file = open("output.dat", "w")
+    
+    for i, line in enumerate(input_file):
+        if i % n == 0:
+            output_file.write(line)
+    
+    input_file.close()
+    output_file.close()
+
+
+
+if __name__ == "__main__":
+    pass
