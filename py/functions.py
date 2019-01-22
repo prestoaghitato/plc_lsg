344d912063edce3402ef95598b6a56ba2eedb00d
Marcel Ruland <ruland.marcel@gmail.com>
2019-01-22 12:20:56 +0100

    add urn functions

--- py/functions.py
+++ py/functions.py
@@ -26,15 +26,38 @@ def simplify(filename, n):
     """
     input_file = open(filename, "r")
     output_file = open("output.dat", "w")
-    
     for i, line in enumerate(input_file):
         if i % n == 0:
             output_file.write(line)
-    
     input_file.close()
     output_file.close()
 
 
+def eat_urns(filename):
+    """
+    import netlogo's urn output into python
+    """
+    out= []
+    urn_file = open(filename, "r")
+    for i, line in enumerate(urn_file):
+        out.append([])
+        for char in line:
+            if char in "012":
+                out[i].append(int(char))
+    return out
+            
+def urn_frequency(urns):
+    out = []
+    for i, urn in enumerate(urns):
+        out.append([0, 0, 0])
+        for ball in urn:
+            out[i][ball] += 1
+    return out
 
 if __name__ == "__main__":
-    pass
+    sender_urns = eat_urns("sender_urns.txt")
+    sender_frequency = urn_frequency(sender_urns)
+    sender_urns = 0
+    receiver_urns = eat_urns("receiver_urns.txt")
+    receiver_frequency = urn_frequency(receiver_urns)
+    receiver_urns = 0
